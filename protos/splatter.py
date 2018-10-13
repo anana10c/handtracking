cb3a8dcbd8908ebb276b24450d85f7efcebce8ed
ivwang46 <ivy.wang121101@gmail.com>
2018-10-13 14:20:01 -0400

    opencv splatter class

--- protos/splatter.py
+++ protos/splatter.py
@@ -19,4 +19,26 @@ class Splatter:
         if self.opacity > 0
             self.opacity -= 0.1
 
-    
+
+'''
+import cv2
+import random
+
+class Splatter:
+    def __init__(self):
+        outline = cv2.imread('splatter-original.png', -1)
+        print(outline.shape)
+        cv2.cvtColor(outline, cv2.COLOR_BGRA2RGBA) #remember to try to convert frame to RGBA also
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        print(color)
+        outline[:, :, 0] = color[0]
+        outline[:, :, 1] = color[1]
+        outline[:, :, 2] = color[2]
+        #outline[:, :, 0:3] = color #not sure if this works reee numpy indexing
+
+    def fade(self):
+        #alter alpha channel in opencv? is 0 or 255 transparent
+        self = self.enhance(opacity)
+        if self.opacity > 0:
+            self.opacity -= 0.1
+'''
