06117e7842e547b203a688419501a27c56bcc163
ivwang46 <ivy.wang121101@gmail.com>
2018-10-13 22:46:17 -0400

    oops

--- splatter.py
+++ splatter.py
@@ -9,7 +9,7 @@ class Splatter:
         print("topleft:", topleft)
         print("bottomright:", bottomright)
         self.outline = cv2.resize(self.outline, (bottomright[0]-topleft[0], bottomright[1]-topleft[1]), interpolation = cv2.INTER_AREA)
-        print("outline:", outline.shape)
+        print("outline:", self.outline.shape)
         cv2.cvtColor(self.outline, cv2.COLOR_BGRA2RGBA) #remember to try to convert frame to RGBA also
         if color == None:
             self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
