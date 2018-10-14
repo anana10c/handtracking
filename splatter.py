0caea0a0efbeda95b7666de7b3adbb243e1e9938
Narwhalish <bryao@ctemc.org>
2018-10-13 23:00:35 -0400

    Resize splat images

--- splatter.py
+++ splatter.py
@@ -6,7 +6,7 @@ class Splatter:
 
     def __init__(self, topleft, bottomright, color=None):
         self.outline = cv2.imread('splatter-original.png', -1)
-        self.outline = cv2.resize(self.outline, (bottomright[0]-topleft[0], bottomright[1]-topleft[1]), interpolation = cv2.INTER_AREA)
+        self.outline = cv2.resize(self.outline, Size(), (bottomright[0]-topleft[0], bottomright[1]-topleft[1]), interpolation = cv2.INTER_AREA)
         cv2.cvtColor(self.outline, cv2.COLOR_BGRA2RGBA) #remember to try to convert frame to RGBA also
         if color == None:
             self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
