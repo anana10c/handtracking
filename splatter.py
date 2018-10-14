b9f8bd0e3c6885228b53b32e8417b6bb4c7ecccc
Anna Cai <anana10c@gmail.com>
2018-10-13 21:55:22 -0400

    fixed opacity

--- splatter.py
+++ splatter.py
@@ -50,5 +50,7 @@ class Splatter:
 
     def fade(self):
         #self.outline[self.outline[:, :, 3] >= 4] -= 4
-        if self.opacity >= 0.05:
+        if self.opacity > 0:
             self.opacity -= 0.05
+        if self.opacity < 0:
+            self.opacity = 0
